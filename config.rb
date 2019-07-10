eb99f6258e72e0915f95bd9eda07fd1f67ed9ace
David Annez <david.annez@gmail.com>
2019-07-10 09:09:53 -0600

    Adding toml file

--- config.rb
+++ config.rb
@@ -75,12 +75,4 @@ configure :build do
 
   # Use relative URLs
   activate :relative_assets
-
-  # Compress PNGs after build
-  # First: gem install middleman-smusher
-  # require "middleman-smusher"
-  # activate :smusher
-
-  # Or use a different image path
-  # set :http_path, "/Content/images/"
 end
