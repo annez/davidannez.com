956b7e47fc87114ae061c9811cc8c7185a9e0d48
David Annez <david.annez@gmail.com>
2019-08-04 13:45:32 +0100

    Adding in some details + styles

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
+import './normalize';
 import './style';
 import { Component } from 'preact';
 
 export default class App extends Component {
 	render() {
 		return (
-			<div>
-				<h1>Hello, World!</h1>
+			<div class='container'>
+				<h1>David Annez</h1>
+        <h2>
+          Head of Product Engineering @&nbsp;
+          <a href='https://www.uswitch.com' rel='noopener noreferer' target='_blank'>uSwitch.com</a>
+        </h2>
+        <p>Leader, business owner and generalist engineer.</p>
+        <p>
+          Founder of{' '}
+          <a href='https://shop.frogbeats.com' rel='noopener noreferer' target='_blank'>
+            Frogbeats
+          </a>{' '}
+          and{' '}
+          <a href='https://customs.frogbeats.com' rel='noopener noreferer' target='_blank'>
+            Frogbeats Customs
+          </a>
+        </p>
+        <ul>
+          <li><a href='https://linkedin.com/davidannez'>LinkedIn</a></li>
+          <li><a href='https://twitter.com/davidannez'>Twitter</a></li>
+          <li><a href='https://github.com/davidannez'>Github</a></li>
+        </ul>
 			</div>
 		);
 	}
